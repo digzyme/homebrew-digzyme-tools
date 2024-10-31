8764c238318b3df5218dec35fbefcbb7df5db06a
鈴木 彦有 <>
2024-10-31 11:36:29 +0900

    [fix] a compiler error for 'psipred'

--- Formula/psipred.rb
+++ Formula/psipred.rb
@@ -9,11 +9,13 @@ class Psipred < Formula
   sha256 "ce4c901c8f152f6e93e4f70dc8079a5432fc64d02bcc0215893e33fbacb1fed2"
   license ""
 
+  depends_on "gsed" => :build
   depends_on "brewsci/bio/blast-legacy"
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
 
+    system "sed", "-e", "s/main\(/int main\(/", "-i", "src/sspred_avpred.c"
     system "make", "-C", "src/"
     system "make", "-C", "src/", "install"
     bin.install Dir["bin/*"]
